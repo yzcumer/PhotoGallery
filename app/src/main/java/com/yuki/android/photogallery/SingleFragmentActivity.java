c932adcf4e301a81d3b24617e2f7430b58036efe
yzcumer <yomacangye@outlook.com>
2017-01-01 19:47:43 +0800

    Initial commit

---
+++ app/src/main/java/com/yuki/android/photogallery/SingleFragmentActivity.java
@@ -0,0 +1,31 @@
+package com.yuki.android.photogallery;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bignerdranch.android.photogallery.R;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R
+                .layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+
+}
